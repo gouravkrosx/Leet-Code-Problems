13c88e0b4b01cf0d5106a4ce89e342c442881d82
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-01-15 19:23:35 +0530

    Time: 150 ms (14.37%), Space: 48.7 MB (5.06%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+       long[] dp = new long[nums.length];
+		Arrays.fill(dp, -1);
+		return (int)MinJumps(nums, 0, dp);
+ 
+    }
+    public static long MinJumps(int[] nums, int vidx, long[] dp) {
+		if (vidx >= nums.length)
+			return Integer.MAX_VALUE;
+
+		if (vidx == nums.length - 1) {
+			return 0;
+		}
+		if (dp[vidx] != -1) {
+			return dp[vidx];
+		}
+
+		long jumps = Integer.MAX_VALUE;
+        //val here is the steps it can take
+		for (int val = vidx+1; val <= vidx+nums[vidx]; val++) {
+            if(nums[vidx]!=0)
+			jumps = Math.min(jumps, MinJumps(nums,val, dp));
+		}
+		jumps+=1;
+		return dp[vidx] = jumps;
+	}
+}

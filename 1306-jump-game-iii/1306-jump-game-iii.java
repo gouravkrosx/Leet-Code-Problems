0502888f6436842c50a0c30ba0a05c537bb59c3b
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-01-15 22:20:39 +0530

    Time: 16 ms (7.47%), Space: 53.9 MB (29.46%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,50 +1,20 @@
 class Solution {
    public boolean canReach(int[] arr, int start) {
-			ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-		int n = arr.length;
-
-		for (int i = 0; i < arr.length; i++) {
-			graph.add(new ArrayList<>());
-		}
-
-		for (int i = 0; i < n; i++) {
-
-			int node = arr[i];
-			int a = (i + arr[i] < n) ? i + arr[i] : -1;
-			int b = (i - arr[i] >= 0) ? i - arr[i] : -1;
-
-			if (a != -1) {
-				graph.get(i).add(a);
-			}
-
-			if (b != -1) {
-				graph.get(i).add(b);
-			}
-
-		}
-
-		Queue<Integer> q = new LinkedList<>();
-
-		q.add(start);
-		boolean[] vis = new boolean[n];
-		vis[start] = true;
-
-		while (!q.isEmpty()) {
-			int node = q.poll();
-			if (arr[node] == 0) {
-				return true;
-			}
-
-			for (int an : graph.get(node)) {
-				if (!vis[an]) {
-					vis[an] = true;
-					q.offer(an);
-				}
-			}
-
-		}
-
-		return false;
-   }
+        int n = arr.length;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        boolean[] visited = new boolean[n];
+        visited[start] = true;
+        while (!q.isEmpty()) {
+            int i = q.poll();
+            if (arr[i] == 0) return true; // found result
+            for (int j : new int[]{i - arr[i], i + arr[i]}) {
+                if (j < 0 || j >= n || visited[j]) continue;
+                q.add(j);
+                visited[j] = true;
+            }
+        }
+        return false;
+    }
 }

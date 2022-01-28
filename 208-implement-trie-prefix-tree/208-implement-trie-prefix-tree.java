bceec9dd40c302f6dfb8ff50d64016f5895e5fc8
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-01-29 01:15:21 +0530

    Time: 79 ms (26.15%), Space: 66.4 MB (11.77%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -0,0 +1,86 @@
+ class Node {
+	Node links[] = new Node[26];
+	boolean flag = false;
+
+	public Node() {
+
+	}
+
+	boolean containsKey(char ch) {
+		return (links[ch - 'a'] != null);
+	}
+
+	Node get(char ch) {
+		return links[ch - 'a'];
+	}
+
+	void put(char ch, Node node) {
+		links[ch - 'a'] = node;
+	}
+
+	void setEnd() {
+		flag = true;
+	}
+
+	boolean isEnd() {
+		return flag;
+	}
+}
+
+class Trie {
+
+    private static Node root;
+    
+    public Trie() {
+        root = new Node();
+    }
+    
+    public void insert(String word) {
+        Node node = root;
+		
+        for (int i = 0; i < word.length(); i++) {
+			if (!node.containsKey(word.charAt(i))) {
+				node.put(word.charAt(i), new Node());
+			}
+			// moves to the reference node.
+			node = node.get(word.charAt(i));
+		}
+		node.setEnd();
+    }
+    
+    public boolean search(String word) {
+        Node node = root;
+		
+        for (int i = 0; i < word.length(); i++) {
+			if (!node.containsKey(word.charAt(i))) {
+				return false;
+			}
+			// moves to the reference node.
+			node = node.get(word.charAt(i));
+		}
+		if (node.isEnd()) {
+			return true;
+		}
+		return false;
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node node = root;
+		for (int i = 0; i < prefix.length(); i++) {
+			if (!node.containsKey(prefix.charAt(i))) {
+				return false;
+			}
+			// moves to the reference node.
+			node = node.get(prefix.charAt(i));
+		}
+		return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */

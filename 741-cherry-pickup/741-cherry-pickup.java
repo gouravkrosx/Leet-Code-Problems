f9fbfa97a1b1d4af5861e353bfdc28c3652ae60b
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-02-03 03:10:04 +0530

    Time: 17 ms (74.25%), Space: 52.2 MB (31.26%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.java
+++ 741-cherry-pickup/741-cherry-pickup.java
@@ -1,50 +1,53 @@
 class Solution {
-public int cherryPickup(int[][] grid) {
-        int[][][] dp = new int[grid.length][grid.length][grid.length];
-
-        return Math.max(0, helper(grid, dp, 0, 0, 0));  // return 0 if there is no path from 0,0 to n-1,n-1
+    public int cherryPickup(int[][] grid) {
+        int n = grid.length;
+        int[][][]dp = new int[n][n][n];
+        
+              
+        return Math.max(0,collectCherries(grid,n,0,0,0,dp));
+        
     }
-
-    // Instead of going once from 0,0 to n-1,n-1 and then back, we simply go twice from 0,0 to n-1,n-1 because every path from n-1,n-1 to 0,0 can be interpreted as a path from 0,0 to n-1,n-1
-    // Note that the one person can never cross the past path of the other person (they can only meet at the same position) so we don't need to worry about one person picking up an already picked up cherry from the past
-    // What does a state represent? dp[r1][c1][c2] represents the max number of cherries that can be collected by 2 people going from r1,c1 and r2,c2 to n-1,n-1
-    // Transitions between states? we collect cherries on current positions of the two people (r1,c1 and r2,c2), then we go through all possible next states and choose the best one (max number of cherries) as the next state (we do this by adding the number of cherries of the best next state to the number of cherries we picked up on the current two positions of the people). In the end, the state dp[0][0][0] will contain the max number of cherries that can be picked up by going from 0,0 to n-1,n-1 and back.
-    private int helper(int[][] grid, int[][][] dp, int r1, int c1, int  c2) {
-
-        // we can deduce r2 because r1 + c1 == r2 + c2, since with each move either r or c of a person gets incremented by exactly one (Manhattan distance to origin stays equal)
-        // this way we reduce the 4D dp problem to a 3D one (we save space by reducing the number of things we store in a state)
+    // we are starting both journies form (0,0) itself and not like one from 0,0 and than return journey from (n-1,n-1)
+    // and this is because the possible path combination from (0,0) to (n-1,n-1) is same as (n-1,n-1) to (0,0) even if there
+    // are thorns (hurdles)
+    public int collectCherries(int[][]grid,int n,int r1,int c1,int c2,int[][][]dp){
+        
+//since ( r1 + c1 = r2 + c2 ) because both starts at the same time and reach the destination (n-1,n-1) at the same time        
         int r2 = r1 + c1 - c2;
-
-        // check if current state is out of bounds or on thorns
-        if (r1 >= grid.length || c1 >= grid.length || r2 >= grid.length || c2 >= grid.length || grid[r1][c1] == -1 || grid[r2][c2] == -1) {
-            return Integer.MIN_VALUE;   // current state should not be included in the solution
+        
+        if(r1>=n ||r2>=n || c1>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1){
+            return Integer.MIN_VALUE;
+        } 
+        
+          
+        if(dp[r1][c1][c2]!=0){
+            return dp[r1][c1][c2];
         }
-
-        // check if we have already computed a solution for this state
-        if (dp[r1][c1][c2] != 0) return dp[r1][c1][c2];
-
-        // check if we reached the end state (note that if r1,c1 reached the end, this implies that r2,c2 also reached the end)
-        if (r1 == grid.length - 1 && c1 == grid.length - 1) {
+        
+        if(r1==n-1 && c1==n-1){
             return grid[r1][c1];
         }
-
-        // compute and return answer for current state
-        int result = grid[r1][c1];
-
-        // in case the second person is on the same position, don't pick up the same cherry twice. Note that r1 == r2 <--> c1 == c2 (eg. they can't be on the same row without also being on the same column) 
-        if (r1 != r2) {
-            result += grid[r2][c2];
+      
+        
+        int cherries = 0;
+        if(r1==r2 && c1==c2){
+            cherries = grid[r1][c1];
+        }else{
+            cherries = grid[r1][c1] + grid[r2][c2];
         }
-
-        // pick best possible next state
-        int bestNextState = Math.max(helper(grid, dp, r1 + 1, c1, c2),  // down, down 
-                                    helper(grid, dp, r1, c1 + 1, c2 + 1));  // right, right    
-        bestNextState = Math.max(bestNextState, helper(grid, dp, r1 + 1, c1, c2 + 1));   // down, right
-        bestNextState = Math.max(bestNextState, helper(grid, dp, r1, c1 + 1, c2));   // right, down
-
-        result += bestNextState;
-        dp[r1][c1][c2] = result;    // store current state such that it can be re-used
-
-        return result;
+        
+        // four ways to explore 
+        int dd = collectCherries(grid,n,r1+1,c1,c2,dp);
+        int dr = collectCherries(grid,n,r1+1,c1,c2+1,dp);
+        int rd = collectCherries(grid,n,r1,c1+1,c2,dp);
+        int rr = collectCherries(grid,n,r1,c1+1,c2+1,dp);
+        
+        int max1 = Math.max(dd,rr);
+        int max2 = Math.max(dr,rd);
+        
+        cherries += Math.max(max1,max2);
+        
+         dp[r1][c1][c2]=cherries;
+        return  cherries; 
     }
 }

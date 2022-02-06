cb4fdb562e2701cbb3ba8f156c6fd6ec82fe5b94
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-02-07 02:10:16 +0530

    Time: 6 ms (86.03%), Space: 42.6 MB (8.47%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,43 +1,16 @@
 class Solution {
-    public int[] maxSubsequence(int[] nums, int k) {
-    
-int n = nums.length;
-		pair[] arr = new pair[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = new pair(nums[i], i);
-		}
-
-		Arrays.sort(arr, (a, b) -> a.val - b.val);
-
-		if (k == 1) {
-			return new int[] { arr[0].val };
-		}
-
-		pair[] temp = new pair[k];
-
-		int j = 0;
-		for (int i = n - 1; i >= n - k; i--) {
-			temp[j] = new pair(arr[i].val, arr[i].idx);
-			j++;
-		}
-
-		Arrays.sort(temp, (a, b) -> a.idx - b.idx);
-
-		int[] res = new int[k];
-		for (int i = 0; i < k; i++) {
-			res[i] = temp[i].val;
-		}
-
-		return res;
-    }
-    
-    class pair{
-        int val;
-        int idx;
-        pair(int v,int i){
-            val=v;
-            idx=i;
+    public int[] maxSubsequence(int[] A, int k) {
+         List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < A.length; i++) {
+            list.add(new int[]{A[i], i});
         }
-    }
-    
+        list.sort((a, b) -> b[0] - a[0]); // sort desc by value
+        List<int[]> subList = list.subList(0, k);
+        subList.sort((a, b) -> a[1] - b[1]); // sort asc by index
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = subList.get(i)[0];
+        }
+        return res;
+    }   
 }

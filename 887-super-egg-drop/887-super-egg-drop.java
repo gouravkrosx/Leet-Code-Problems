bde3ae16444f0d6ae76d14d4f33ba998f1c49987
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-02-11 22:23:42 +0530

    Time: 8 ms (82.52%), Space: 43 MB (59.07%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.java
+++ 887-super-egg-drop/887-super-egg-drop.java
@@ -1,39 +1,13 @@
 class Solution {
-    public int superEggDrop(int k, int n) {
-        int[][]dp=new int[k+1][n+1];
-        
-        for(int[]arr:dp){
-            Arrays.fill(arr,-1);
+    public int superEggDrop(int K, int N) {
+       int[][] dp = new int[N + 1][K + 1];
+        int m = 0;
+        while (dp[m][K] < N) {
+            ++m;
+            for (int k = 1; k <= K; ++k)
+                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
         }
-        
-        return eggdrop(k,n,dp);
+        return m;
     }
-    public int eggdrop(int eggs,int floors,int[][]dp){
-        if(eggs==1||floors==1){
-            return floors;
-        }
-        
-        if(floors<=0){
-            return 0;
-        }
-        if(dp[eggs][floors]>0)return dp[eggs][floors];
-        
-        int min=100000;
-        int l=1;
-        int h=floors;
-        
-        while(l<=h){
-            int mid = (l+h)/2;
-            int left = eggdrop(eggs-1,mid-1,dp);
-            int right = eggdrop(eggs,floors-mid,dp);
-            int temp = 1 + Math.max(left,right);
-            if(left<right){  // since right is more than left and we need more in worst case 
-                l = mid+1;   // so l=mid+1 to gain more temp for worst case : upward
-            }else{
-                h = mid-1;
-            }
-            min = Math.min(min,temp);
-        }
-        return dp[eggs][floors]=min;
-    }
+   
 }

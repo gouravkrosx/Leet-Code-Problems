6a6c76a5457585a170fed8ad17c8ae614ea415c5
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-02-15 15:52:13 +0530

    Time: 4 ms (63.81%), Space: 52.5 MB (11.59%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,18 +1,20 @@
 class Solution {
+    
+  // Best explaination-> 
+    // https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/discuss/964331/Intuition-Explained-Simple-Code-O(N)-time-O(1)-space-Java
+    
     public int numPairsDivisibleBy60(int[] time) {
-    if(time.length<=1)return 0;
-        
-    int ans = 0;
-        
-    Map<Integer,Integer>count = new HashMap<>();     
-        
-    for (int t : time) {
-    int reducedTime = t % 60;
-    int other = (reducedTime == 0) ? 0 : 60 - reducedTime;
-    ans += count.getOrDefault(other, 0);
-    count.put(reducedTime, count.getOrDefault(reducedTime, 0) + 1);
-   }
-        
+   int remainder[]=new int[60];
+        int n=time.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int rem=time[i]%60;
+            if(rem==0)
+                ans+=remainder[0];
+            else
+                ans+=remainder[60-rem];
+            remainder[rem]++;
+        }
         return ans;
     }
 }

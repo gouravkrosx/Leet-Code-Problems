e88b916080f8d9c1edf1fac156310832f6d7de8d
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-02-24 03:56:17 +0530

    Added solution - LeetHub

---
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.java
@@ -0,0 +1,171 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String arr[] = br.readLine().split(" ");
+	            int k = Integer.parseInt(arr[0]);
+	            int node = Integer.parseInt(arr[1]);
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Tree g = new Tree();
+        		System.out.println(g.kthAncestor(root,k,node));
+                    t--;
+            
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+Structure of Node class is:
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+
+class Tree
+{
+    public int kthAncestor(Node root, int k, int node)
+    {
+        
+        int[] K = new int[]{k};
+        Node ans = KthAncestor(root,K,node);
+        
+    
+        if(ans==null || ans.data==node)return -1;
+        else return ans.data;
+    }
+    
+    public Node KthAncestor(Node root,int[] k,int tnode){
+         if(root==null)return null;
+         
+         if(root.data==tnode){
+             return root;
+         }
+         
+         Node left = KthAncestor(root.left,k,tnode);
+         Node right = KthAncestor(root.right,k,tnode);
+         
+         if(left!=null && right==null){
+             k[0]--;
+             if(k[0]<=0){
+                 //resetting k to lock the ans, since when k became 0 we got our ans.
+                 // so now setting to a value so that it never reaches 0.
+                 k[0] = Integer.MAX_VALUE;
+                 return root;
+             }
+             return left;
+         }
+         
+         if(left==null && right!=null){
+             k[0]--;
+             if(k[0]<=0){
+                 //resetting k to lock the ans, since when k became 0 we got our ans.
+                 // so now setting to a value so that it never reaches 0.
+                 k[0] = Integer.MAX_VALUE;
+                 return root;
+             }
+             return right;
+         }
+         return null;
+    }
+}

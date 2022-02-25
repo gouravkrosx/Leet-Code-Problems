1c5e8d3812ce24cb6ad3f50e900be59bfc778064
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-02-26 00:14:39 +0530

    Time: 1 ms (76.03%), Space: 43.9 MB (16.89%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int l = 0;
+		int r = arr.length - 1;
+		while (l <= r) {
+			int mid = (l + r) / 2;
+			if (arr[mid] == target) {
+				return true;
+			}
+			// left part sorted
+			if (arr[mid] > arr[l]) {
+				// key lies in left part
+				if (target < arr[mid] && target >= arr[l]) {
+					r = mid;
+				} else {
+					l = mid + 1;
+				}
+			} else if(arr[mid]<arr[l]) {// right part sorted
+				if (target > arr[mid] && target <= arr[r]) {
+					l = mid + 1;
+				} else {
+					r = mid;
+				}
+			}else{
+                l++; // have no idea about the array, but we can exclude nums[l] because nums[l] == nums[mid]
+            }
+		}
+		return false;
+    }
+}

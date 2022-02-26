5b906db4e3e813e61b5c21bd558d026d4c1968a3
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-02-27 01:47:36 +0530

    Time: 191 ms (14.71%), Space: 72.4 MB (14.16%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int shortestPathLength(int[][] graph) {
+        int mask = 1, n = graph.length;
+        
+        Queue<int[]>q = new LinkedList<>();
+        Set<String>visPath = new HashSet<>();
+        
+        for(int i=0;i<n;i++){
+            mask = (mask | 1<<i);
+            q.offer(new int[]{i,1<<i});
+            visPath.add(i+":"+(1<<i));
+        }
+        
+        int count = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int[]rnode = q.poll();
+                if(rnode[1]==mask)return count;
+                
+                for(int node:graph[rnode[0]]){
+                    int next = rnode[1] | (1<<node);
+                    if(visPath.contains(node+":"+next))continue;
+                    visPath.add(node+":"+next);
+                    q.offer(new int[]{node,next});
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+}

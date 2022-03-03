e87415ee069bd8824c359167e2f3a0f5dc6b461e
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-03 21:03:45 +0530

    Time: 3 ms (93.65%), Space: 81.6 MB (9.49%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxArea(int[] heights) {
+       int maxArea = 0;
+        
+        int left = 0;
+        int right = heights.length - 1;
+        
+        int maxHeight = 0;
+        
+        while (left <= right) {
+            // because the length is always decreasing
+            // we only need to consider a pair if its height
+            // is greater than any pair's height we've used so far
+            if (heights[left] > maxHeight && heights[right] > maxHeight) {
+                maxHeight = Math.min(heights[left], heights[right]);
+                maxArea = Math.max(maxArea, maxHeight * (right - left));
+            }
+            
+            // move pointers
+            if (heights[left] == heights[right]) {
+                left ++;
+                right --;
+            } else if (heights[left] < heights[right]) {
+                left ++;
+            } else {
+                right --;
+            }
+        }
+        
+        return maxArea; 
+    }
+}

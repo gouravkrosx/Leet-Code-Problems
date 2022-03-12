815865d1e6a6a3d2699d200dca8c59bf8ad9f0d9
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-12 22:48:29 +0530

    Time: 22 ms (70.75%), Space: 65.1 MB (27.56%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -6,24 +6,25 @@ class Solution {
         int m = grid[0].length;
         
         Queue<int[]>q = new LinkedList<>();
-        boolean vis[][] = new boolean[n][m];
-        int[][]dp = new int[n][m];
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     q.add(new int[]{i,j});
-                    vis[i][j] = true;
                 }
             }
         }
         
+        if(q.size()==n*m)return -1;
+        
         int max = -1;
-        boolean allOnes = true;
+
         int[][]direction = {{0,1},{1,0},{0,-1},{-1,0}};
         
         while(!q.isEmpty()){
+            max++;
             int size = q.size();
+            
             for(int i=0;i<size;i++){
                 int[]arr = q.poll();
                 int r = arr[0];
@@ -32,13 +33,8 @@ class Solution {
                     int nr = r + direction[dir][0];
                     int nc = c + direction[dir][1];
                  
-                     if(isValid(vis,nr,nc)){
-                        int dis = getDis(r,c,nr,nc);
-                         
-                        dp[nr][nc] = dis + dp[r][c];
-                         
-                        max = Math.max(dp[nr][nc],max); 
-                        vis[nr][nc]=true;
+                     if(isValid(grid,nr,nc)){
+                        grid[nr][nc]=1;
                         q.add(new int[]{nr,nc});
                     }
                 }
@@ -46,11 +42,8 @@ class Solution {
         }
         return max;
     }
-    public boolean isValid(boolean[][]vis,int i,int j){
-        if(i<0||i>=vis.length||j<0||j>=vis[0].length||vis[i][j])return false;
+    public boolean isValid(int[][]grid,int i,int j){
+        if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]==1)return false;
         return true;
-    }
-    public int getDis(int i1,int j1,int i2,int j2){
-        return (Math.abs(i1-i2)+Math.abs(j1-j2));
     }
 }

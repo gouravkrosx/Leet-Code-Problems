ac67abeb5a771f4a53df7863860da1aefa461679
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-17 01:31:40 +0530

    Time: 552 ms (7.40%), Space: 190.6 MB (5.16%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.java
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.java
@@ -1,30 +1,29 @@
 class Solution {
     public int lenLongestFibSubseq(int[] arr) {
+       
+        int n = arr.length;
+        int[][]dp = new int[n][n];
         HashMap<Integer,Integer>map = new HashMap<>();
         
         int max = 0;
         
-        for(int val:arr){
-            map.put(val,0);
-        }
-        
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                int a = arr[i];
-                int b = arr[j];
-                int c = a+b;
-                map.put(a,1);
-                map.put(b,2);
-                
-                while(map.containsKey(c)){
-                    map.put(c,map.get(b)+1);
-                    max = Math.max(max,map.get(c));
-                    a = b;
-                    b = c;
-                    c = a+b;
+        for(int i=0;i<n;i++){
+            
+            map.put(arr[i],i);
+            
+            for(int j=0;j<i;j++){
+                Integer k = map.get(arr[i]-arr[j]);
+                // the number arr[i]-arr[j] should be smaller than arr[j] because arr[j] is second number and 
+                // if k is found then arr[k] will be first number.
+                if(arr[i]-arr[j]<arr[j] && k!=null){
+                    dp[i][j] = dp[j][k]+1; 
+                    max = Math.max(max,dp[i][j]);
+                }else{
+                    dp[i][j] = 2;
                 }
             }
         }
-        return max;
+        
+        return (max>2)?max:0;
     }
 }

ea09d4aabcee2acb2566691c1533802ccf412571
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-18 19:07:16 +0530

    Time: 5 ms (60.22%), Space: 43.2 MB (25.30%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,36 +1,33 @@
 class Solution {
-    public String removeDuplicateLetters(String s) {
-        Stack<Character>st = new Stack<>();
-        
-        HashMap<Character,Integer>map = new HashMap<>();
-        Set<Character>set = new HashSet<>();
-        
-        for(char ch:s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-                       
-            if(set.contains(ch)){
-                map.put(ch,map.get(ch)-1);
-                continue;
-            }
-            
-            while(!st.isEmpty() && st.peek()>ch && map.get(st.peek())>1){                
-                map.put(st.peek(),map.get(st.peek())-1);
-                set.remove(st.pop());
-            }
-            set.add(ch);
-            st.push(ch);
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
-        
-        return sb.reverse().toString();
+   public String removeDuplicateLetters(String sr) {
+
+    int[] res = new int[26]; //will contain number of occurences of character (i+'a')
+    boolean[] visited = new boolean[26]; //will contain if character (i+'a') is present in current result Stack
+    char[] ch = sr.toCharArray();
+    for(char c: ch){  //count number of occurences of character 
+        res[c-'a']++;
     }
+    Stack<Character> st = new Stack<>(); // answer stack
+    int index;
+    for(char s:ch){ 
+        index= s-'a';
+        res[index]--;   //decrement number of characters remaining in the string to be analysed
+        if(visited[index]) //if character is already present in stack, dont bother
+            continue;
+        //if current character is smaller than last character in stack which occurs later in the string again
+        //it can be removed and  added later e.g stack = bc remaining string abc then a can pop b and then c
+        while(!st.isEmpty() && s<st.peek() && res[st.peek()-'a']!=0){ 
+            visited[st.pop()-'a']=false;
+        }
+        st.push(s); //add current character and mark it as visited
+        visited[index]=true;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    //pop character from stack and build answer string from back
+    while(!st.isEmpty()){
+        sb.insert(0,st.pop());
+    }
+    return sb.toString();
+}
 }

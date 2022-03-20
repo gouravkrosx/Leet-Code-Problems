79604d5c2e6bafc8a9d9d2d70732a9d9a256a882
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-20 18:55:14 +0530

    Time: 2 ms (83.25%), Space: 41.5 MB (62.09%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
@@ -1,31 +1,21 @@
 class Solution {
-    public int mctFromLeafValues(int[] arr) { 
-     
-        int n = arr.length;
-     
-        int[][][]dp = new int[n][n][2];
-        return  maxCountTree(arr,0,n-1,dp)[0];
-    }
-//     {minSum,maxLeaf}
-    public int[] maxCountTree(int[]arr,int si,int ei,int[][][]dp){
-        if(si==ei){
-            return new int[]{0,arr[si]};
+    public int mctFromLeafValues(int[] arr) {
+        int res=0;
+        Stack<Integer>st = new Stack<>();
+        st.push(Integer.MAX_VALUE);
+        
+        for(int a:arr){
+            while(st.peek()<=a){
+                int mid = st.pop();
+                res+= mid * Math.min(st.peek(),a);
+            }
+            st.push(a);
         }
         
-        if(dp[si][ei][0]!=0)return dp[si][ei];
-    
-        int ans = Integer.MAX_VALUE;
-        int maxLeaf = 0;
-        
-        for(int k=si;k<ei;k++){
-            int[] left = maxCountTree(arr,si,k,dp);
-            int[] right = maxCountTree(arr,k+1,ei,dp);
-            
-            maxLeaf = Math.max(maxLeaf,Math.max(left[1],right[1]));
-            
-            int overAll = left[1]*right[1] + left[0] + right[0];
-            ans = Math.min(ans,overAll);
+        while(st.size()>2){
+            res+=(st.pop()*st.peek());
         }
-        return dp[si][ei]=new int[]{ans,maxLeaf};
+        
+        return res;
     }
 }

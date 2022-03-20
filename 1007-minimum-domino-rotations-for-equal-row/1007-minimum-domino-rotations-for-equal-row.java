4a476119c562aca59448223bdc8233814e076e05
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-20 11:44:12 +0530

    Time: 3 ms (99.81%), Space: 50 MB (80.90%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,42 +1,27 @@
 class Solution {
-    public int minDominoRotations(int[] tops, int[] bottoms) {
-        
-        int min=Integer.MAX_VALUE;
-        for(int dice=1;dice<=6;dice++){
-            int c=0;
-            int r=0;
-            for(int i=0;i<tops.length;i++){
-                if(tops[i]==dice){
-                    c++;
-                }else if(bottoms[i]==dice){
-                    c++;
-                    r++;
-                }
-            }    
-            
-            if(c==tops.length){
-                min = Math.min(min,r);
-            }
+/*
+If it is possible to create a row of identical numbers, a[0] or b[0] must be in this solution.
+If a[0] and b[0] are both in the solution, they are either the same number or there are only two numbers in all dominoes
+
+A = [1,1,1,1]
+B = [1,1,1,1]
+or
+A = [1,2,1,2]
+B = [2,1,2,1]
+
+*/    
+   public int minDominoRotations(int[] A, int[] B) {
+        int n = A.length;
+        for (int i = 0, a = 0, b = 0; i < n && (A[i] == A[0] || B[i] == A[0]); ++i) {
+            if (A[i] != A[0]) a++;
+            if (B[i] != A[0]) b++;
+            if (i == n - 1) return Math.min(a, b);
         }
-        
-        for(int dice=1;dice<=6;dice++){
-            int c=0;
-            int r=0;
-            for(int i=0;i<bottoms.length;i++){
-                if(bottoms[i]==dice){
-                    c++;
-                }else if(tops[i]==dice){
-                    c++;
-                    r++;
-                }
-            }    
-            
-            if(c==bottoms.length){
-                min = Math.min(min,r);
-            }
+        for (int i = 0, a = 0, b = 0; i < n && (A[i] == B[0] || B[i] == B[0]); ++i) {
+            if (A[i] != B[0]) a++;
+            if (B[i] != B[0]) b++;
+            if (i == n - 1) return Math.min(a, b);
         }
-        
-        
-        return (min==Integer.MAX_VALUE)?-1:min;
+        return -1;
     }
 }

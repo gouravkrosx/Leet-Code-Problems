62ad6d215b17d5085a8417a045d7d5722608e45f
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-21 17:54:52 +0530

    Time: 2 ms (99.79%), Space: 41.1 MB (75.60%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,49 +1,24 @@
 class Solution {
-    public List<Integer> partitionLabels(String s) {
-        HashMap<Character,Integer>start = new HashMap<>();
-        HashMap<Character,Integer>end = new HashMap<>();
-        
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(!start.containsKey(ch)){
-                start.put(ch,i+1);
+    public List<Integer> partitionLabels(String S) {
+          if(S == null || S.length() == 0){
+            return null;
+        }
+        List<Integer> list = new ArrayList<>();
+        int[] map = new int[26];  // record the last index of the each char
+
+        for(int i = 0; i < S.length(); i++){
+            map[S.charAt(i)-'a'] = i;
+        }
+        // record the end index of the current sub string
+        int last = 0;
+        int start = 0;
+        for(int i = 0; i < S.length(); i++){
+            last = Math.max(last, map[S.charAt(i)-'a']);
+            if(last == i){  //it means the max end has been reached in the curr substring hence it can be a partition
+                list.add(last - start + 1);
+                start = last + 1;
             }
-            end.put(ch,i+1);
         }
-        
-        List<int[]>list = new ArrayList<>();
-        
-        for(char key:start.keySet()){
-            int st = start.get(key);
-            int e = end.get(key);
-            
-            list.add(new int[]{st,e});
-        }
-        Collections.sort(list,(a,b)->(a[0]-b[0]));
-        
-        int[] prev = list.get(0);
-        int i=0;
-        int j=1;
-        
-        while(j<list.size()){
-            if(prev[1]>=list.get(j)[0]){
-                list.get(i)[1] = Math.max(list.get(j)[1],prev[1]);
-            }else{
-                i++;
-                list.get(i)[0]=list.get(j)[0];
-                list.get(i)[1]=list.get(j)[1];
-                prev = list.get(i);
-            }
-            j++;
-        }     
-        
-        List<Integer>ans = new ArrayList<>();
-        
-        for(int idx=0;idx<=i;idx++){
-            int val = list.get(idx)[1]-list.get(idx)[0]+1;
-            ans.add(val);
-        }
-        
-        return ans;
+        return list;
     }
 }

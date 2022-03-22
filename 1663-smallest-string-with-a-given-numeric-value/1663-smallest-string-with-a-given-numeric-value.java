1679c1ba6c96d76fb290662ceacfcdee5801cbf4
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-22 19:37:38 +0530

    Time: 87 ms (18.52%), Space: 66.5 MB (26.46%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
@@ -1,9 +1,14 @@
 class Solution {
+//Idea: Basically checking from end so that you take max value character and at last you only need to take less value character which leads to smallest string in lexicographically order.    
     public String getSmallestString(int n, int k) {
        
         StringBuilder sb = new StringBuilder();
         
         while(k>0){
+            // consider that _ _ _ _ at the 4th place you want to put a character, for this you also consider for
+            // 0-3 characters, assume you put only a there which has 1 value so so won't take any character if its
+            // greater than equal to current k value, along with this you also take prev characters acc
+            // that's why (k-(n-1)).
             if(k-(n-1)>=26){
                 sb.append("z");
                 k-=26;

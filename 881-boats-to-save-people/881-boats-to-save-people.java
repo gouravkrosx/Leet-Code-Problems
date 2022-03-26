0bb20731e135e5c36ce80521460018679db52a24
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-03-27 03:46:34 +0530

    Time: 7 ms (97.80%), Space: 66.3 MB (7.79%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -1,9 +1,21 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int i, j;
-        for (i = 0, j = people.length - 1; i <= j; --j)
-            if (people[i] + people[j] <= limit) ++i;
-        return people.length - 1 - j;
+       int[] buckets = new int[limit+1];
+        for(int p: people) buckets[p]++;
+        int start = 0;
+        int end = buckets.length - 1;
+        int solution = 0;
+        while(start <= end) {
+            //make sure the start always point to a valid number
+            while(start <= end && buckets[start] <= 0) start++;
+            //make sure end always point to valid number
+            while(start <= end && buckets[end] <= 0) end--;
+            //no one else left on the ship, hence break.
+            if(buckets[start] <= 0 && buckets[end] <= 0) break;
+            solution++;
+            if(start + end <= limit) buckets[start]--; // both start and end can carry on the boat
+            buckets[end]--;
+        }
+        return solution;
     }
 }

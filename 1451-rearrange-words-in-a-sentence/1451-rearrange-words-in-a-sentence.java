b333df438fa6d6ed6b067d3966e0d40f5dabdf20
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-04-01 21:54:10 +0530

    Time: 52 ms (34.81%), Space: 54.4 MB (50.44%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
@@ -1,35 +1,8 @@
 class Solution {
     public String arrangeWords(String text) {
-        
-        String[] str = text.split(" ");
-        
-        Arrays.sort(str,new Comparator<>(){
-           @Override
-            public int compare(String a,String b){
-                if(a.length()==b.length()){
-                    return 0;
-                }else{
-                    return a.length()-b.length();
-                }
-            }
-        });
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0;i<str.length;i++){
-            String s = str[i];
-            char ch = s.charAt(0);
-            String ros = s.substring(1);    
-            if(i==0){
-                sb.append(Character.toUpperCase(ch)+""+ros+" ");
-            }else if(i<str.length-1){
-                sb.append(Character.toLowerCase(ch)+""+ros+" ");
-            }else{
-                sb.append(Character.toLowerCase(ch)+""+ros);
-            }
-            
-        }
-        
-      
-        return sb.toString();
+        String []s = text.toLowerCase().split(" ");
+        Arrays.sort(s, (a,b) ->  a.length() - b.length());
+        String ans = String.join(" ", s);
+        return Character.toUpperCase(ans.charAt(0)) + ans.substring(1);
     }
 }

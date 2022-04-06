385a6829aa059b49a8171b486194390b2d2deb05
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-04-06 20:50:15 +0530

    Time: 1848 ms (5.24%), Space: 144.2 MB (6.05%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,35 +1,19 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
-        long[] count = new long[101];
+       long res=0;
+       long mod = 1000000007;
+       
+        Map<Integer,Integer>map = new HashMap<>();
         
-        //to count the occurences of numbers.
-        for(int i=0;i<arr.length;i++)count[arr[i]]++;
+        for(int i=0;i<arr.length;i++){
+            res=(res+map.getOrDefault(target-arr[i],0))%mod;
             
-        long res = 0; 
-        long mod = 1000000007;
-        
-        for(int i=0;i<101;i++){
-            for(int j=i;j<101;j++){
-                int k = target - i - j;
-                
-                if(k>100 || k<0)continue;
-                
-                //nC3
-                if(i==j && j==k){
-                    res+=(count[i]*(count[i]-1)*(count[i]-2))/6;
-                }
-                
-                //nC2
-                else if(i==j && j!=k){
-                    res+=((count[i]*(count[i]-1)/2)*(count[k]));
-                }
-                
-                else if(i<j && j<k){
-                    res+=(count[i]*count[j]*count[k]);
-                }
-                res=res%mod;
+            for(int j=0;j<i;j++){
+                int sum = arr[i]+arr[j];
+                map.put(sum,map.getOrDefault(sum,0)+1);
             }
         }
+        
         return (int)res;
     }
 }

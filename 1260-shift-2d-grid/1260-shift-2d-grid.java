d5a2c5811ccf854f1ff50894565db9b50ac5d555
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-04-11 14:09:36 +0530

    Time: 9 ms (52.71%), Space: 54.2 MB (75.58%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,45 +1,18 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
-        
-        k = k%(m*n);
-        
-        List<Integer>list = new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                list.add(grid[i][j]);
+         List<List<Integer>> ans = new ArrayList<>();
+        int row = grid.length;
+        int col = grid[0].length;
+        int elements = row * col;
+        List<Integer> list = new ArrayList<>();
+        k = k % elements;
+        for(int i=elements-k;i<elements*2-k;i++){
+            list.add(grid[(i%elements)/col][(i%elements)%col]);
+            if(list.size()%col==0){
+                ans.add(list);
+                list = new ArrayList<>();
             }
-        }
-        
-        reverse(list,0,(m*n)-k-1);
-        reverse(list,(m*n)-k,(m*n)-1);
-        reverse(list,0,(m*n)-1);
-        
-        List<List<Integer>>ans = new ArrayList<>();
-        
-        int id = 0;
-        while(id<(m*n)){
-            List<Integer>temp = new ArrayList<>();
-            int jd = 0;
-            while(jd<n){
-                temp.add(list.get(id+jd));
-                jd++;
-            }
-            id+=n;
-            ans.add(temp);
         }
         return ans;
-    }
-    public void reverse(List<Integer>list,int l,int h){
-              
-        while(l<h){
-            int a = list.get(l);
-            int b = list.get(h);
-            list.set(l,b);
-            list.set(h,a);
-            l++;
-            h--;
-        }
     }
 }
